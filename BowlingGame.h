4c5221371597291222fad31180bc7f76e2821bdd
kuser <kuser>
2022-04-27 19:38:20 +0200

    Task done

--- BowlingGame.h
+++ BowlingGame.h
@@ -1,10 +1,72 @@
 #pragma once
+#include <exception>
+class BowlingException : public std::exception
+{
+	const char * what () const throw ()
+    {
+    	return "Bowling Exception";
+    }
+};
 
+const int MAX_ROUNDS = 10;
 class BowlingGame
 {
-public:
-	void roll(int pins) {}
-	int getScore() {}
-private:
+	public:
+	void roll(int p) {
+		if (p > pins) {
+			throw new BowlingException();
+		}
+		if (round > MAX_ROUNDS) {
+			if (!(firstBonus||secondBonus)) {
+				throw new BowlingException();
+			}
+			int k=0;
+			if(firstBonus) k++;
+			if(secondBonus) k++;
+			score += k*p;
+			addBonus(0);
+			pins -= p;
+			if(p==10) {
+				pins = 10;
+			}
+		} else {
+			addPoints(p);
+			pins -= p;
+			num++;
+			if (p == 10) {
+				addBonus(2);
+			} else if (pins == 0) {
+				addBonus(1);
+			} else {
+				addBonus(0);
+			}
+			if(!(num%2)||p==10) {
+				num = 0;
+				round++;
+				pins = 10;
+			}
+		}
+	}
+	int getScore() {
+		return score;
+	}
+	private:
+	void addPoints(int p) {
+		int k=1;
+		if (firstBonus) k++;
+		if (secondBonus) k++;
+		score += p*k;
+	}
+	void addBonus(int b) {
+		secondBonus = firstBonus - 1;
+		if (secondBonus < 0) secondBonus = 0;
+		firstBonus = b;
+	}
+	int num = 0;
+	int firstBonus = 0;
+	int secondBonus = 0;
+	int score = 0;
+	int pins = 10;
+	int round = 1;
 };
 
--- BowlingTDDProject.cpp
+++ BowlingTDDProject.cpp
@@ -1,16 +1,105 @@
 #include "catch/catch.hpp"
 #include "BowlingGame.h"
 
-class SimpleCalculator {
-public:
-    int add(int a, int b) {
-        return a + b;
+TEST_CASE("Simple_once") {
+    BowlingGame bg;
+    bg.roll(4);
+    REQUIRE( bg.getScore() == 4);
+}
+TEST_CASE("Simple_round") {
+    BowlingGame bg;
+    bg.roll(4);
+    bg.roll(5);
+    REQUIRE( bg.getScore() == 9);
+}
+TEST_CASE("Two rounds") {
+    BowlingGame bg;
+    bg.roll(4);
+    bg.roll(5);
+    bg.roll(4);
+    bg.roll(5);
+    REQUIRE( bg.getScore() == 18);
+}
+TEST_CASE("Strike") {
+    BowlingGame bg;
+    bg.roll(10);
+    REQUIRE( bg.getScore() == 10);
+}
+TEST_CASE("Spare") {
+    BowlingGame bg;
+    bg.roll(5);
+    bg.roll(5);
+    REQUIRE( bg.getScore() == 10);
+}
+TEST_CASE("Spare boost") {
+    BowlingGame bg;
+    bg.roll(5);
+    bg.roll(5);
+    bg.roll(5);
+    bg.roll(1);
+    REQUIRE( bg.getScore() == 21);
+}
+TEST_CASE("Strike boost") {
+    BowlingGame bg;
+    bg.roll(10);
+    bg.roll(5);
+    bg.roll(5);
+    REQUIRE( bg.getScore() == 30);
+}
+TEST_CASE("Strike full boost") {
+    BowlingGame bg;
+    bg.roll(10);
+    bg.roll(10);
+    bg.roll(10);
+    bg.roll(0);
+    bg.roll(0);
+    REQUIRE( bg.getScore() == 60);
+}
+TEST_CASE("Strike full boost 10 rounds") {
+    BowlingGame bg;
+    for (int i=0;i<12;i++)
+    {
+        bg.roll(10);
     }
-};
-
-TEST_CASE("SimpleCalculator_add_test") {
-    SimpleCalculator testObj;
-
-    REQUIRE( testObj.add(2, 2) == 4);
-    REQUIRE( testObj.add(2, 4) == 6);
+    REQUIRE( bg.getScore() == 300);
+}
+TEST_CASE("10x10 + 2 + 8") {
+    BowlingGame bg;
+    for (int i=0;i<10;i++)
+    {
+        bg.roll(10);
+    }
+    bg.roll(2);
+    bg.roll(8);
+    REQUIRE( bg.getScore() == 282);
+}
+TEST_CASE("11x10 + 0") {
+    BowlingGame bg;
+    for (int i=0;i<11;i++)
+    {
+        bg.roll(10);
+    }
+    bg.roll(0);
+    REQUIRE( bg.getScore() == 290);
+}
+TEST_CASE("10x10 + 0 + 0") {
+    BowlingGame bg;
+    for (int i=0;i<10;i++)
+    {
+        bg.roll(10);
+    }
+    bg.roll(0);
+    bg.roll(0);
+    REQUIRE( bg.getScore() == 270);
+}
+TEST_CASE("9x10 + 8 + 2 + 1") {
+    BowlingGame bg;
+    for (int i=0;i<9;i++)
+    {
+        bg.roll(10);
+    }
+    bg.roll(8);
+    bg.roll(2);
+    bg.roll(1);
+    REQUIRE( bg.getScore() == 269);
 }
